a10e69f04cee3a78fc3b976b76ee274046a28e95
Mathieu Glachant <syvwlch@gmail.com>
2012-03-22 18:18:39 -0400

    Turns out, the print command doesn't NEED parentheses.
    They are optional, and so this is a simpler way to code the same result.

--- ps0.py
+++ ps0.py
@@ -2,8 +2,8 @@
 # Name: Mathieu Glachant
 # Collaborators: None
 # Time Spent: 0:22
-print('Enter your date of birth')
+print 'Enter your date of birth'
 dob=raw_input('**')
-print('Enter your last name:')
+print 'Enter your last name:'
 name=raw_input('**')
-print(name+' '+dob)
+print name+' '+dob
