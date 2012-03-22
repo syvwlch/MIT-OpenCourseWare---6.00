99926b9ceb050d3eafb8cd740f205ea8629f2ada
Mathieu Glachant <syvwlch@gmail.com>
2012-03-22 18:19:17 -0400

    Added blank lines for readibility

--- ps0.py
+++ ps0.py
@@ -2,8 +2,11 @@
 # Name: Mathieu Glachant
 # Collaborators: None
 # Time Spent: 0:22
+
 print 'Enter your date of birth'
 dob=raw_input('**')
+
 print 'Enter your last name:'
 name=raw_input('**')
+
 print name+' '+dob
