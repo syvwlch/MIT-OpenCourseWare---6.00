ba6ca1baf20ebe58be69e9f744918a6b71ba0dad
Mathieu Glachant <syvwlch@gmail.com>
2012-03-23 14:24:32 -0400

    Threw out the stuff not needed
    Made monthly payment a variable, not an input
    Threw out the prints in the for loop
    Threw out the running total calculation and display

--- ps1b.py
+++ ps1b.py
@@ -9,28 +9,19 @@ outstanding_balance=float(raw_input('Enter the outstanding balance'
                               ' on your credit card: '))
 annual_interest_rate=float(raw_input('Enter the annual credit card interest rate'
                                ' as a decimal: '))
-minimum_monthly_payment_rate=float(raw_input('Enter the minimum monthly payment rate'
-                                       ' as a decimal: '))
+# Initialize some variables
+minimum_monthly_payment=10.0
 
 # Iterating the calculation and display of the results
-total_amount_paid=0.0
 for month in range(1,13):
     # Calculating the month
-    minimum_monthly_payment=round(minimum_monthly_payment_rate
-                                  *outstanding_balance, 2)
     interest_paid=round(annual_interest_rate/12*outstanding_balance, 2)
     principal_paid=minimum_monthly_payment-interest_paid
     outstanding_balance=outstanding_balance-principal_paid
-    total_amount_paid=total_amount_paid+minimum_monthly_payment
-
-    # Printing the month
-    print 'Month: '+str(month)
-    print 'Minimum monthly payment: $'+str(minimum_monthly_payment)
-    print 'Principle paid: $'+str(principal_paid)
-    print 'Remaining balance: $'+str(outstanding_balance)
 
 # Calculate and print the results over the year
 
 print 'RESULT'
-print 'Total amount paid: $'+str(total_amount_paid)
+print 'Monthly payment to pay off debt in one year: '+str(minimum_monthly_payment)
+print 'Number of months needed: '+str(1)
 print 'Remaining balance: $'+str(outstanding_balance)
