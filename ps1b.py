8f0c5327311659f9971537944617e2b5c5575966
Mathieu Glachant <syvwlch@gmail.com>
2012-03-23 14:34:08 -0400

    Added while loop on outstanding balance
    Works, but the for loop still runs for twelve months, even if the
    balance is paid in fewer months than that.

--- ps1b.py
+++ ps1b.py
@@ -5,23 +5,28 @@
 #
 
 # Gathering user inputs
-outstanding_balance=float(raw_input('Enter the outstanding balance'
+initial_balance=float(raw_input('Enter the outstanding balance'
                               ' on your credit card: '))
 annual_interest_rate=float(raw_input('Enter the annual credit card interest rate'
                                ' as a decimal: '))
 # Initialize some variables
-minimum_monthly_payment=10.0
+minimum_monthly_payment=0.0
+outstanding_balance=initial_balance
 
-# Iterating the calculation and display of the results
-for month in range(1,13):
-    # Calculating the month
-    interest_paid=round(annual_interest_rate/12*outstanding_balance, 2)
-    principal_paid=minimum_monthly_payment-interest_paid
-    outstanding_balance=outstanding_balance-principal_paid
+# Iterating until the minimum payment is sufficient to pay off the debt
+while outstanding_balance>0:
+    minimum_monthly_payment=minimum_monthly_payment+10.0
+    outstanding_balance=initial_balance
+    # Iterating the calculation and display of the results
+    for month in range(1,13):
+        # Calculating the month
+        interest_paid=round(annual_interest_rate/12*outstanding_balance, 2)
+        principal_paid=minimum_monthly_payment-interest_paid
+        outstanding_balance=outstanding_balance-principal_paid
 
 # Calculate and print the results over the year
 
 print 'RESULT'
 print 'Monthly payment to pay off debt in one year: '+str(minimum_monthly_payment)
-print 'Number of months needed: '+str(1)
+print 'Number of months needed: '+str(month)
 print 'Remaining balance: $'+str(outstanding_balance)
