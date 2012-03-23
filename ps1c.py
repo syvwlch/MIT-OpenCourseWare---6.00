63837763920a40fe959a75266e4502193c9d4332
Mathieu Glachant <syvwlch@gmail.com>
2012-03-23 19:11:15 -0400

    Bisection
    Iterate while upper and lower bounds are more than one cent apart, and
    if outstanding balance is positive after one year, the lower bound
    moves up, else the upper bound moves down.

--- ps1c.py
+++ ps1c.py
@@ -10,26 +10,34 @@ initial_balance=float(raw_input('Enter the outstanding balance'
 annual_interest_rate=float(raw_input('Enter the annual credit card interest rate'
                                ' as a decimal: '))
 # Initialize some variables
-minimum_monthly_payment=0.0
+lower_bound_monthly_payment=initial_balance/12.0
+upper_bound_monthly_payment=(
+    initial_balance*(1+(annual_interest_rate/12))**12.0)/12.0
 outstanding_balance=initial_balance
 
-# Iterating until the minimum payment is sufficient to pay off the debt
-while outstanding_balance>0:
-    minimum_monthly_payment=minimum_monthly_payment+10.0
+# Iterating until the minimum payment is enough to pay the debt in one year
+while upper_bound_monthly_payment-lower_bound_monthly_payment>=0.01:
+    minimum_monthly_payment=(
+        (lower_bound_monthly_payment+upper_bound_monthly_payment)/2.0)
     outstanding_balance=initial_balance
     month=0
-    # Iterating each month while it's less than a year AND the debt is not paid
-    while month<12 and outstanding_balance>0:
-        month=month+1
+    # Iterating each month while it's less than a year
+    for month in range(1,13):
         # Calculating the month
         interest_paid=round(annual_interest_rate/12*outstanding_balance, 2)
         principal_paid=minimum_monthly_payment-interest_paid
         outstanding_balance=outstanding_balance-principal_paid
-
+    # check to see if we are over or under the ideal value
+    if outstanding_balance>0:
+        lower_bound_monthly_payment=minimum_monthly_payment
+    else:
+        upper_bound_monthly_payment=minimum_monthly_payment
+    
 # Calculate and print the results over the year
 
 print 'RESULT'
 print 'Monthly payment to pay off debt in one year: '+str(
-            minimum_monthly_payment)
+            round(minimum_monthly_payment, 2))
 print 'Number of months needed: '+str(month)
-print 'Remaining balance: $'+str(outstanding_balance)
+print 'Remaining balance: $'+str(
+            round(outstanding_balance, 2))
