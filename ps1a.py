3c7135f931def2f6a42126e023e696e98d41d791
Mathieu Glachant <syvwlch@gmail.com>
2012-03-23 12:32:40 -0400

    Added the user input section
    Some lines are more than 80 chars long, need to research how to
    continue them in a properly Python way.

--- ps1a.py
+++ ps1a.py
@@ -3,3 +3,8 @@
 # Collaborators: None
 # Time Spent: 0 (started 12:15 on Friday)
 #
+
+# Gathering user inputs
+outstanding_balance=raw_input('Enter the outstanding balance on your credit card: ')
+annual_interest_rate=raw_input('Enter the annual credit card interest rate as a decimal: ')
+minimum_monthly_payment_rate=raw_input('Enter the minimum monthly payment rate as a decimal: ')
