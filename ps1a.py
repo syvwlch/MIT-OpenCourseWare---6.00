5eb394ce10652a0ee3b7549710b6db8fa94ff3d7
Mathieu Glachant <syvwlch@gmail.com>
2012-03-23 13:12:20 -0400

    Added the running total paid and the remaining balance

--- ps1a.py
+++ ps1a.py
@@ -13,6 +13,7 @@ minimum_monthly_payment_rate=float(raw_input('Enter the minimum monthly payment 
                                        ' as a decimal: '))
 
 # Iterating the calculation and display of the results
+total_amount_paid=0.0
 for month in range(1,13):
     # Calculating the month
     minimum_monthly_payment=round(minimum_monthly_payment_rate
@@ -20,9 +21,16 @@ for month in range(1,13):
     interest_paid=round(annual_interest_rate/12*outstanding_balance, 2)
     principal_paid=minimum_monthly_payment-interest_paid
     outstanding_balance=outstanding_balance-principal_paid
+    total_amount_paid=total_amount_paid+minimum_monthly_payment
 
     # Printing the month
     print 'Month: '+str(month)
     print 'Minimum monthly payment: $'+str(minimum_monthly_payment)
     print 'Principle paid: $'+str(principal_paid)
     print 'Remaining balance: $'+str(outstanding_balance)
+
+# Calculate and print the results over the year
+
+print 'RESULT'
+print 'Total amount paid: $'+str(total_amount_paid)
+print 'Remaining balance: $'+str(outstanding_balance)
